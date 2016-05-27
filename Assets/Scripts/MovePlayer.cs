623b702336ad6344356a7b401bcecd061bf14604
leeeennyy <alena.choong@gmail.com>
2016-05-28 00:32:38 +1200

    Add project to github

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public float Speed = 0.2f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position -= new Vector3(Speed, 0.0f, 0.0f);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(Speed, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/TextImporter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextImporter : MonoBehaviour
+{
+    public GameObject textbox;
+    public Text text;
+
+    public TextAsset textFile;
+    public string[] lines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public MovePlayer player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<MovePlayer>();
+
+        if (textFile != null)
+        {
+            lines = textFile.text.Split('\n');
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
