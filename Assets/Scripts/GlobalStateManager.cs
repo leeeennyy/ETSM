f38756d40742467b55d5d65de83a3f99cd7a6eea
leeeennyy <alena.choong@gmail.com>
2016-06-18 12:58:21 +1200

    Added user interaction with door
    Also changed spaces to tabs

--- Assets/Scripts/GlobalStateManager.cs
+++ Assets/Scripts/GlobalStateManager.cs
@@ -4,48 +4,48 @@ using System.Collections;
 
 public class GlobalStateManager : MonoBehaviour
 {
-    private static GlobalStateManager instance;
+	private static GlobalStateManager instance;
 
-    public static GlobalStateManager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                instance = GameObject.FindObjectOfType<GlobalStateManager>();
-                DontDestroyOnLoad(instance.gameObject);
-            }
+	public static GlobalStateManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<GlobalStateManager>();
+				DontDestroyOnLoad(instance.gameObject);
+			}
 
-            return instance;
-        }
-    }
+			return instance;
+		}
+	}
 
-    public int Counter = 0;
-    public Text Label;
-    public Vector3 LastUsedDoorPosition;
+	public int Counter = 0;
+	public Text Label;
+	public Vector3 LastUsedDoorPosition;
 
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this);
-        }
-        else if (instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-    }
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else if (instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+	}
 
-    // Use this for initialization
-    void Start()
-    {
-        Label.text = Counter.ToString();
-    }
-	
-    // Update is called once per frame
-    void Update()
-    {
-        Label.text = Counter.ToString();
-    }
+	// Use this for initialization
+	void Start()
+	{
+		Label.text = Counter.ToString();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Label.text = Counter.ToString();
+	}
 }
--- Assets/Scripts/PlayerCollisionManager.cs
+++ Assets/Scripts/PlayerCollisionManager.cs
@@ -3,40 +3,63 @@ using System.Collections;
 
 public class PlayerCollisionManager : MonoBehaviour
 {
-    public GameObject ChatDialog;
+	public GameObject ChatDialog;
 
-    // Use this for initialization
-    void Start()
-    {
-	
-    }
-	
-    // Update is called once per frame
-    void Update()
-    {
-	
-    }
+	// Use this for initialization
+	void Start()
+	{
 
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Npc")
-        {
-            ChatDialog.SetActive(true);
-        }
-        else if (collider.gameObject.name == "DoorToRoom")
-        {
-            // change the scene
-            GlobalStateManager.Instance.LastUsedDoorPosition = collider.gameObject.transform.localPosition;
-            Application.LoadLevel(1);
-            transform.localPosition = GlobalStateManager.Instance.LastUsedDoorPosition;
-        }
-        else if (collider.gameObject.name == "DoorToMain")
-        {
-            GlobalStateManager.Instance.LastUsedDoorPosition = collider.gameObject.transform.localPosition;
-            
-            // change the scene
-            Application.LoadLevel(0);
-            transform.localPosition = GlobalStateManager.Instance.LastUsedDoorPosition;
-        }
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		var go = collider.gameObject;
+
+		if (go.tag == "Npc")
+		{
+			ChatDialog.SetActive(true);
+		}
+		else if (go.tag == "Door")
+		{
+			if (Input.GetKey(KeyCode.E))
+			{
+				if (go.name == "DoorToRoom")
+				{
+					MovePlayerToLastDoorPosition(1, collider.gameObject.transform.localPosition);
+				}
+				else if (go.name == "DoorToMain")
+				{
+					MovePlayerToLastDoorPosition(0, collider.gameObject.transform.localPosition);
+				}
+			}
+		}
+
+	}
+
+	/// <summary>
+	/// Moves the player to the last door position
+	/// </summary>
+	/// <param name="level">The new scene we want to load</param>
+	/// <param name="doorPosition">The new door position</param>
+	private void MovePlayerToLastDoorPosition(int level, Vector3 doorPosition)
+	{
+		if (GlobalStateManager.Instance.LastUsedDoorPosition == null)
+		{
+			GlobalStateManager.Instance.LastUsedDoorPosition = doorPosition;
+			Application.LoadLevel(level);
+			transform.localPosition = GlobalStateManager.Instance.LastUsedDoorPosition;
+		}
+		else
+		{
+			transform.localPosition = GlobalStateManager.Instance.LastUsedDoorPosition;
+			Application.LoadLevel(level);
+			GlobalStateManager.Instance.LastUsedDoorPosition = doorPosition;
+		}
+	}
 }
