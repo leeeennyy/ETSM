cb080d02824b5bd152eeaf9941f28b549deb6197
NZJenkins <a.miles.nz@gmail.com>
2016-08-01 23:46:38 +1200

    Added condition node
    With example of how they might work
    No evaluator/vars done yet

---
+++ Assets/Scripts/Interaction Tree/Nodes/ConditionNode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using NodeEditorFramework;
+
+[Node(false, "Condition Node")]
+public class ConditionNode : Node {
+    public override string GetID { get { return "ConditionNode"; } }
+    public string Condition = "";
+
+    public override Node Create(Vector2 pos) {
+        ConditionNode node = CreateInstance<ConditionNode>();
+        node.rect = new Rect(pos.x, pos.y, 200, 60);
+        node.name = "Condition Node";
+
+        node.CreateInput("Previous", "Default", NodeSide.Top);
+
+        node.CreateOutput("Else", "Default", NodeSide.Right);
+        node.CreateOutput("Next", "Default", NodeSide.Bottom);
+
+        return node;
+    }
+
+    protected override void NodeGUI() {
+        GUILayout.BeginHorizontal();
+        Condition = GUILayout.TextField(Condition);
+        GUILayout.EndHorizontal();
+    }
+}
