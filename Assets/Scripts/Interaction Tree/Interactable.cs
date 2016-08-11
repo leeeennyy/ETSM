e58511bf90506fa234abc6060b16b2a996ffae1e
NZJenkins <a.miles.nz@gmail.com>
2016-08-11 13:02:35 +1200

    Added Lua Interpreter project

--- Assets/Scripts/Interaction Tree/Interactable.cs
+++ Assets/Scripts/Interaction Tree/Interactable.cs
@@ -1,19 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MoonSharp.Interpreter;
 
 public class Interactable : MonoBehaviour {
 
     public NodeEditorFramework.NodeCanvas InteractionTree;
     public string Root = "";
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
 
